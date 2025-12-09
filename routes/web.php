80796cc3beb6858e06bb073fb2bc20fdbcb8c22d
Gilmar <gilmarpereira1212@hotmail.com>
2025-12-08 22:31:39 -0300

    Sistema de cadastro

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ Route::prefix('/login-sys')->name('login-sys.')->controller(\App\Http\Controller
     Route::post('/login',[App\Http\Controllers\Api\LoginApiController::class,'login'])->name('login');
     Route::post('/logout',[App\Http\Controllers\Api\LoginApiController::class,'logout'])->name('logout');
 });
+Route::prefix('/register')->name('register.')->controller(\App\Http\Controllers\Api\LoginApiController::class)->group(function(){
+    Route::post('/register',[App\Http\Controllers\RegisterController::class,'store'])->name('store-sys');
+    Route::post('/logout',[App\Http\Controllers\RegisterController::class,'registerView'])->name('register');
+});
 Route::prefix('/blog')->name('blog.')->controller(\App\Http\Controllers\BlogController::class)->group( function () {
     Route::get('/', 'index')->name('home');
     Route::delete('/delete/{id}', 'destroy')->name('delete');
